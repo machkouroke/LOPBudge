bff54669d7b48402d49bf1ecfff7163075df76f7
OKE   Machkour <40785379+machkouroke@users.noreply.github.com>
2026-07-11 04:13:21 +0200

    fix(main): remove SplashScreen API call to fix unresolved reference

--- app/src/main/java/com/lop/budget/MainActivity.kt
+++ app/src/main/java/com/lop/budget/MainActivity.kt
@@ -3,7 +3,6 @@ package com.lop.budget
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.lop.budget.ui.navigation.LopNavHost
 import com.lop.budget.ui.theme.LopBudgeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,7 +10,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         setContent {
